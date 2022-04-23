6ed07fc5d7e9f17a51f55605b89d6a7cc0e77057
ZhakPaganelle <daniilprozorkevich@mail.ru>
2022-04-23 13:09:40 +0300

    Made finding group in parser

---
+++ app/parser.py
@@ -0,0 +1,67 @@
+"""Parser for origin schedule website"""
+import requests
+from bs4 import BeautifulSoup
+
+HEADERS = {
+    'x-requested-with': 'XMLHttpRequest',
+}
+
+
+def get_options(name: str, data: dict[str, str]) -> list[str]:
+    """Requests page and returns all options' values from 
+    the selector with given name"""
+    url = 'https://rasp.rea.ru/Schedule/Navigator'
+    r = requests.post(url=url, headers=HEADERS, data=data)
+    if r.status_code != 200:
+        raise RuntimeError('Couldn\'t access the site')
+    soup = BeautifulSoup(r.text, "html.parser")
+    faculties = soup.find(attrs={"name": name})
+    return [option['value'] for option in faculties.find_all('option') if option['value'] not in ('na', '')]
+
+
+def get_faculties() -> list[str]:
+    """Returns list of all faculties"""
+    data = {
+        'ChangedNode': 'Faculty',
+    }
+    name = 'Faculty'
+    return get_options(name, data)
+
+
+def get_cathedras() -> list[str]:
+    """Returns list of all cathedras"""
+    data = {
+        'ChangedNode': 'Cathedra',
+    }
+    name = 'Cathedra'
+    return get_options(name, data)
+
+
+def get_courses(faculty: str) -> list[str]:
+    data = {
+        'Faculty': faculty,
+        'ChangedNode': 'Faculty',
+    }
+    name = 'Course'
+    return get_options(name, data)
+
+
+def get_study_types(faculty: str, course: str = '1-й курс') -> list[str]:
+    data = {
+        'Faculty': faculty,
+        'Course': course,
+        'ChangedNode': 'Course',
+    }
+    name = 'Type'
+    return get_options(name, data)
+
+
+def get_groups(faculty: str, course: str, study_type: str) -> list[str]:
+    data = {
+        'Faculty': faculty,
+        'Course': course,
+        'Type': study_type,
+        'ChangedNode': 'Type',
+    }
+    name = 'Group'
+    return get_options(name, data)
