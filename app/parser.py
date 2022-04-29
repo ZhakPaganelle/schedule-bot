0cc2b056b80d281d2ae82f33328949f8d4720281
ZhakPaganelle <daniilprozorkevich@mail.ru>
2022-04-29 22:06:22 +0300

    Added async to parser

--- app/parser.py
+++ app/parser.py
@@ -1,6 +1,8 @@
 """Parser for origin schedule website"""
 import re
-import requests
+import time
+import aiohttp
+import asyncio
 from bs4 import BeautifulSoup
 
 LessonInfo = list[dict[str | int, int | str]]
@@ -12,50 +14,52 @@ HEADERS = {
 }
 
 
-def get_options(name: str, data: dict[str, str]) -> list[str]:
+async def get_options(name: str, data: dict[str, str]) -> list[str]:
     """Requests page and returns all options' values from
     the selector with given name"""
     url = 'https://rasp.rea.ru/Schedule/Navigator'
-    req = requests.post(url=url, headers=HEADERS, data=data)
-    if req.status_code != 200:
-        raise RuntimeError('Couldn\'t access the site')
-    soup = BeautifulSoup(req.text, "html.parser")
-    faculties = soup.find(attrs={"name": name})
-    return [
-        option['value'] for option in faculties.find_all('option')
-        if option['value'] not in ('na', '')
-    ]
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url=url, headers=HEADERS, data=data) as req:
+            if req.status != 200:
+                raise RuntimeError('Couldn\'t access the site')
+            text = await req.text()
+            soup = BeautifulSoup(text, "html.parser")
+            faculties = soup.find(attrs={"name": name})
+            return [
+                option['value'] for option in faculties.find_all('option')
+                if option['value'] not in ('na', '')
+            ]
 
 
-def get_faculties() -> list[str]:
+async def get_faculties() -> list[str]:
     """Returns list of all faculties"""
     data = {
         'ChangedNode': 'Faculty',
     }
     name = 'Faculty'
-    return get_options(name, data)
+    return await get_options(name, data)
 
 
-def get_cathedras() -> list[str]:
+async def get_cathedras() -> list[str]:
     """Returns list of all cathedras"""
     data = {
         'ChangedNode': 'Cathedra',
     }
     name = 'Cathedra'
-    return get_options(name, data)
+    return await get_options(name, data)
 
 
-def get_courses(faculty: str) -> list[str]:
+async def get_courses(faculty: str) -> list[str]:
     """Returns list of courses based on faculty"""
     data = {
         'Faculty': faculty,
         'ChangedNode': 'Faculty',
     }
     name = 'Course'
-    return get_options(name, data)
+    return await get_options(name, data)
 
 
-def get_study_types(faculty: str, course: str = '1-й курс') -> list[str]:
+async def get_study_types(faculty: str, course: str = '1-й курс') -> list[str]:
     """Returns type of study (bachelor, master or specialist"""
     data = {
         'Faculty': faculty,
@@ -63,10 +67,10 @@ def get_study_types(faculty: str, course: str = '1-й курс') -> list[str]:
         'ChangedNode': 'Course',
     }
     name = 'Type'
-    return get_options(name, data)
+    return await get_options(name, data)
 
 
-def get_groups(faculty: str, course: str, study_type: str) -> list[str]:
+async def get_groups(faculty: str, course: str, study_type: str) -> list[str]:
     """Returns list of groups from course
     based on faculty, year and studying type"""
     data = {
@@ -76,33 +80,47 @@ def get_groups(faculty: str, course: str, study_type: str) -> list[str]:
         'ChangedNode': 'Type',
     }
     name = 'Group'
-    return get_options(name, data)
+    return await get_options(name, data)
 
 
-def get_schedule(group: str, week_num: int = -1, detailed: bool = False) -> list[dict[str | int, int | str]]:
-    """Returns list of days with lessons
-    Every day is also a list"""
-    pass
+async def get_lesson_info(selection: str, date: str, time_slot: int) -> LessonInfo:
+    url = 'https://rasp.rea.ru/Schedule/GetDetails'
+    params = {'selection': selection.lower(), 'date': date, 'timeSlot': time_slot}
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=url, headers=HEADERS, params=params) as req:
+            soup = BeautifulSoup(await req.text(), "html.parser")
 
-
-def get_lesson_info(group: str, date: str, time_slot: int) -> LessonInfo:
-    params = {'selection': group.lower(), 'date': date, 'timeSlot': time_slot}
-    req = requests.get('https://rasp.rea.ru/Schedule/GetDetails', headers=HEADERS, params=params)  # type: ignore
-    soup = BeautifulSoup(req.text, "html.parser")
     lessons = [div for div in soup.find_all('div') if div.attrs.get('data-subgroup')]
 
     subjects = [lesson.find('h5').text for lesson in lessons]
 
     types = [lesson.find('strong').text for lesson in lessons]
 
-    rooms = [re.findall(r'Аудитория:\s+(\d+) \w+ -\s+(\d+)\s', str(lesson))[0] for lesson in lessons]
+    rooms_raw = [re.findall(r'Аудитория:\s+(\d+) \w+ -\s+(\d+)\s', str(lesson)) for lesson in lessons]
+    rooms = [room[0] if room else room for room in rooms_raw]
     teachers = [re.findall(r'</i> ([\w ]+)</a>', str(lesson)) for lesson in lessons]
-    groups = [re.findall(r'Группа\s+([\w\./\- \d\(\)]+)\s+<br/>', str(lesson))[0] for lesson in lessons]
+    groups_raw = [re.findall(r'Группа\s+([\w\./\- \d\(\)]+)\s+<br/>', str(lesson)) for lesson in lessons]
+    groups = [group[0] if group else group for group in groups_raw]
 
     contents = zip(subjects, types, rooms, teachers, groups)
-    headings = ('subject', 'type', 'room', 'teachers', 'subgroup')
+    headings = ('subject', 'type', 'room', 'teachers', 'group')
     return [dict(zip(headings, lesson)) for lesson in contents]  # type: ignore
 
 
-if __name__ == '__main__':
-    print(get_lesson_info('15.06д-э03а/19б', '26.04.2022', 5))
+async def get_day_info(selection: str, date: str) -> DayInfo:
+    lessons = asyncio.gather(*[get_lesson_info(selection, date, lesson_index) for lesson_index in range(1, 9)])
+    return dict(zip(range(1, 9), await lessons))
+
+
+async def get_week_info(selection: str, week_num: int = -1) -> WeekInfo:
+    dates = await get_dates(selection, week_num)
+    days = asyncio.gather(*[get_day_info(selection, date) for date in dates])
+    return dict(zip(dates, await days))
+
+
+async def get_dates(selection: str, week_num: int = -1) -> set[str]:
+    url = 'https://rasp.rea.ru/Schedule/ScheduleCard'
+    params = {'selection': selection.lower(), 'weekNum': week_num}
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=url, headers=HEADERS, params=params) as schedule:
+            return set(re.findall(r'\d{2}\.\d{2}\.\d{4}', await schedule.text()))
