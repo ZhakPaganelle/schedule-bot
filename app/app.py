b6e6670eaf840e3e2bd5af6a1ae9a171068e2af2
ZhakPaganelle <daniilprozorkevich@mail.ru>
2022-05-01 23:26:30 +0300

    Changed style

--- app/app.py
+++ app/app.py
@@ -1,92 +1,92 @@
 """Parsed API from rasp.rea.ru"""
 from fastapi import FastAPI, responses
-import starlette.status as status
+from starlette import status
 
-import parser
+from app import schedule_parser
 
-app = FastAPI()
+application = FastAPI()
 
 
-@app.get("/hello/", response_model=str)
+@application.get("/hello/", response_model=str)
 async def hello():
     """Test function"""
     return "Hello2"
 
 
-@app.get("/api/faculties/")
+@application.get("/api/faculties/")
 async def get_faculties():
-    return await parser.get_faculties()
+    return await schedule_parser.get_faculties()
 
 
-@app.get("/api/faculties/{faculty}")
+@application.get("/api/faculties/{faculty}")
 async def get_courses(faculty: str):
-    return await parser.get_courses(faculty)
+    return await schedule_parser.get_courses(faculty)
 
 
-@app.get("/api/faculties/{faculty}/{course}")
+@application.get("/api/faculties/{faculty}/{course}")
 async def get_types(faculty: str, course: str):
-    return await parser.get_study_types(faculty, course)
+    return await schedule_parser.get_study_types(faculty, course)
 
 
-@app.get("/api/faculties/{faculty}/{course}/{study_type}")
+@application.get("/api/faculties/{faculty}/{course}/{study_type}")
 async def get_groups(faculty: str, course: str, study_type: str):
-    return await parser.get_groups(faculty, course, study_type)
+    return await schedule_parser.get_groups(faculty, course, study_type)
 
 
-@app.get("/api/faculties/{faculty}/{course}/{study_type}/{group:path}")
-async def redirect_to_group_schedule(faculty: str, course: str, study_type: str, group: str):  # type: ignore
+@application.get("/api/faculties/{faculty}/{course}/{study_type}/{group:path}")
+async def redirect_to_group_schedule(group: str):
     return responses.RedirectResponse(
         f'/api/schedule/{group}',
         status_code=status.HTTP_302_FOUND)
 
 
-@app.get("/api/cathedras/")
+@application.get("/api/cathedras/")
 async def get_cathedras():
-    return await parser.get_cathedras()
+    return await schedule_parser.get_cathedras()
 
 
-@app.get("/api/cathedras/{cathedra}")
+@application.get("/api/cathedras/{cathedra}")
 async def get_teachers(cathedra: str):
-    return await parser.get_teachers(cathedra)
+    return await schedule_parser.get_teachers(cathedra)
 
 
-@app.get("/api/cathedras/{cathedra}/{teacher}")
-async def redirect_to_teacher_schedule(cathedra: str, teacher: str):  # type: ignore
+@application.get("/api/cathedras/{cathedra}/{teacher}")
+async def redirect_to_teacher_schedule(teacher: str):
     return responses.RedirectResponse(
         f'/api/schedule/{teacher}',
         status_code=status.HTTP_302_FOUND)
 
 
-@app.get('/api/schedule/{selection:path}/date/{date:str}')
+@application.get('/api/schedule/{selection:path}/date/{date:str}')
 async def send_schedule_date(selection: str, date: str):
-    return await parser.get_day_info(selection, date)
+    return await schedule_parser.get_day_info(selection, date)
 
 
-@app.get('/api/schedule/{selection:path}/date/{date:str}/lesson/{lesson_id}')
+@application.get('/api/schedule/{selection:path}/date/{date:str}/lesson/{lesson_id}')
 async def send_lesson_by_date(selection: str, date: str, lesson_id: int):
-    return await parser.get_lesson_info(selection, date, lesson_id)
+    return await schedule_parser.get_lesson_info(selection, date, lesson_id)
 
 
-@app.get('/api/schedule/{selection:path}/week/{week_num}/day/{day_num:int}')
-async def send_schedule_week(selection: str, week_num: int, day_num: int):
+@application.get('/api/schedule/{selection:path}/week/{week_num}/day/{day_num:int}')
+async def send_schedule_day_by_week(selection: str, week_num: int, day_num: int):
     selection = selection.lower()
-    dates = await parser.get_dates(selection, week_num)
+    dates = await schedule_parser.get_dates(selection, week_num)
     date = dates[day_num - 1]
-    return await parser.get_day_info(selection, date)
+    return await schedule_parser.get_day_info(selection, date)
 
 
-@app.get('/api/schedule/{selection:path}/week/{week_num}')
+@application.get('/api/schedule/{selection:path}/week/{week_num}')
 async def send_schedule_week(selection: str, week_num: int):
-    return await parser.get_week_info(selection.lower(), int(week_num))
+    return await schedule_parser.get_week_info(selection.lower(), int(week_num))
 
 
-@app.get('/api/schedule/{selection:path}/week/{week_num}/day/{day_num:int}/lesson/{lesson_id}')
+@application.get('/api/schedule/{selection:path}/week/{week_num}/day/{day_num}/lesson/{lesson_id}')
 async def send_lesson_by_week(selection: str, week_num: int, day_num: int, lesson_id: int):
-    dates = await parser.get_dates(selection, week_num)
+    dates = await schedule_parser.get_dates(selection, week_num)
     date = dates[day_num - 1]
-    return await parser.get_lesson_info(selection, date, lesson_id)
+    return await schedule_parser.get_lesson_info(selection, date, lesson_id)
 
 
-@app.get('/api/schedule/{selection:path}')
+@application.get('/api/schedule/{selection:path}')
 async def send_schedule(selection: str):
-    return await parser.get_week_info(selection)
+    return await schedule_parser.get_week_info(selection)
